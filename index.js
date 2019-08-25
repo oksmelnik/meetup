8d468e2dc908f4e6e3d9fb7e76426f4096d4d46a
Oksana Melnik <melnik.oksana@gmail.com>
2019-08-25 19:55:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,66 +3,76 @@ var mup = new Meetup()
 var app = require('express')()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
-server.listen(3002)
+
+const bodyParser = require('body-parser')
+const Router = require('express').Router
+
+server.listen(3002, () =>console.log('Express API listening on port 3002'))
+app.use(bodyParser.json());
 
 var topicsCounter = {}
 
 
+const router = new Router()
+
+this.country = 'nl'
+
+app.get('/country/:id', (req, res) => {
+    this.country = req.params.id
+    res.send(200)
+})
+
 mup.stream("/2/rsvps", stream => {
-  stream
-    .on("data", item => {
-      console.log('------------------')
+    stream.on("data", item => {
 
-      // inside of our stream event handler (!) we retrieve the group topics
-      const topicNames = item.group.group_topics.map(topic => topic.topic_name)
-
-      if (topicNames.includes('Software Development')) {
-        topicNames.forEach(name => {
-          if (topicsCounter[name]) {
-            topicsCounter[name]++
-          }
-          else {
-            topicsCounter[name] = 1
-          }
-        })
-
-        const arrayOfTopics = Object.keys(topicsCounter)
-
-        arrayOfTopics.sort((topicA, topicB) => {
-          if (topicsCounter[topicA] > topicsCounter[topicB]) {
-            return -1
-          }
-          else if (topicsCounter[topicB] > topicsCounter[topicA]) {
-            return 1
-          }
-          else {
-            return 0
-          }
-        })
-
-        const top10 = arrayOfTopics.slice(0, 10)
-
-        const topicsCounts = top10.map(topic => ({
-          topic,
-          count: topicsCounter[topic]
-        }))
-
-  io.emit('action',
-  {type: 'ADD_RSVP',
-  payload: item})
-
-  io.emit('action',
-  {type: 'UPDATE_TOPIC',
-  payload: topicsCounts})
-
-        console.log(topicsCounts)
-      }
+        // inside of our stream ev ent handler (!) we retrieve the group topics
+        const topicNames = item.group.group_topics.map(topic => topic.topic_name)
 
 
-    }
-  ).on("error", e => {
-       console.log("error! " + e)
+        if (item.group.group_country === this.country) {
+
+            console.log('------------------')
+            console.log(item.group)
+
+            topicNames.forEach(name => {
+                if (topicsCounter[name]) {
+                    topicsCounter[name]++
+                } else {
+                    topicsCounter[name] = 1
+                }
+            })
+
+            const arrayOfTopics = Object.keys(topicsCounter)
+
+            arrayOfTopics.sort((topicA, topicB) => {
+                if (topicsCounter[topicA] > topicsCounter[topicB]) {
+                    return -1
+                }
+                else if (topicsCounter[topicB] > topicsCounter[topicA]) {
+                    return 1
+                } else {
+                    return 0
+                }
+            })
+
+            const top10 = arrayOfTopics.slice(0, 10)
+
+            const topicsCounts = top10.map(topic => ({
+              topic,
+              count: topicsCounter[topic]
+            }))
+
+            io.emit('action', {
+                type: 'ADD_RSVP',
+                payload: item
+            })
+
+            io.emit('action', {
+                type: 'UPDATE_TOPIC',
+                payload: topicsCounts
+            })
+        }
+    }).on("error", e => {
+        console.log("error! " + e)
     });
-
-
-    });
+});
